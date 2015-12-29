d00d1f545e4afbdc80c17f8ac63b508df8e5373d
Rene Sprietsma <spriet4000@Renes-iMac.local>
2015-12-29 18:21:27 +0100

    clean

--- src/test/java/com/github/spriet2000/handlers/tests/BenchMark1.java
+++ src/test/java/com/github/spriet2000/handlers/tests/BenchMark1.java
@@ -1,22 +1,22 @@
 package com.github.spriet2000.handlers.tests;
 
+import com.github.spriet2000.handlers.BiHandlers;
 import com.github.spriet2000.handlers.Handlers;
 import org.openjdk.jmh.annotations.Benchmark;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-import static com.github.spriet2000.handlers.Handlers.compose;
 
 public class BenchMark1 {
 
     @Benchmark
-    public void report() {
-
+    public void reportHandlers() {
         AtomicBoolean hitException = new AtomicBoolean(false);
         AtomicBoolean hitComplete = new AtomicBoolean(false);
 
-        Handlers<StringBuilder> handlers = compose(
+        Handlers<StringBuilder> handlers = Handlers.compose(
                 (f, n) -> n::accept,
                 (f, n) -> n::accept,
                 (f, n) -> n::accept);
@@ -28,6 +28,24 @@ public class BenchMark1 {
         for (int i = 0; i < 10000; i++) {
             handler.accept(new StringBuilder());
         }
+    }
 
+    @Benchmark
+    public void reportBiHandlers() {
+        AtomicBoolean hitException = new AtomicBoolean(false);
+        AtomicBoolean hitComplete = new AtomicBoolean(false);
+
+        BiHandlers<StringBuilder, Void> handlers = BiHandlers.compose(
+                (f, n) -> (e, a) -> n.accept(a),
+                (f, n) -> (e, a) -> n.accept(a),
+                (f, n) -> (e, a) -> n.accept(a));
+
+        BiConsumer<StringBuilder, Void> handler = handlers.apply(
+                (e, a) -> hitException.set(true),
+                (e, a) -> hitComplete.set(true));
+
+        for (int i = 0; i < 10000; i++) {
+            handler.accept(new StringBuilder(), null);
+        }
     }
 }
--- src/test/java/com/github/spriet2000/handlers/tests/BenchMarksRunner.java
+++ src/test/java/com/github/spriet2000/handlers/tests/BenchMarksRunner.java
@@ -1,6 +1,5 @@
 package com.github.spriet2000.handlers.tests;
 
-import org.openjdk.jmh.profile.StackProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -13,7 +12,6 @@ public class BenchMarksRunner {
     public static void main(String... args) throws IOException, RunnerException {
         Options opt = new OptionsBuilder()
                 .include(".*" + BenchMark1.class.getSimpleName() + ".*")
-                .addProfiler(StackProfiler.class)
                 .forks(1)
                 .build();
         new Runner(opt).run();
